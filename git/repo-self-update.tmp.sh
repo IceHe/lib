061e443e3e36f4ff9b5543f4868c45030e6511b2
IceHe.xyz <icehe.xyz@qq.com>
2018-09-07 16:42:42 +0800

    Add git/repo-self-update.tmp.sh

---
+++ git/repo-self-update.tmp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "
+================================================================================
+START: $(date)"
+
+#echo '
+#>> export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+#export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+echo '
+>> export'
+export
+
+echo '
+>> cd /path/to/repo && pwd'
+cd /path/to/repo && pwd
+
+echo '
+>> git stash && git stash clear'
+git stash && git stash clear
+
+echo '
+>> git checkout master'
+git checkout master
+
+echo '
+>> git fetch -apv'
+git fetch -apv
+
+echo '
+>> git pull
+'
+git pull
+
+echo "
+FINISH: $(date)
+================================================================================
+"
