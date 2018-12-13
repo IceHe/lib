eecbcc9d175fd6170f2973610ba5e25cb43212e5
IceHe.xyz <icehe.xyz@qq.com>
2018-12-13 21:00:05 +0800

    Update snips/redis/redis-hmset.sh

--- snips/redis/redis-hmset.sh
+++ snips/redis/redis-hmset.sh
@@ -9,6 +9,7 @@ step=25
 for i in `seq 1000 $step 1075`; do
     beg=`expr $i + 1`
     end=`expr $i + $step`
+    # TODO: 不能这么写，每个 php 进程不同，不同 process 重新调用 redis-cli 命令，会重新建一个连接，可用的端口可能用完！
     cmd="php redis-hmset.php $beg $end | tee -a hmset-cmds-$beg-$end"
     echo $cmd
     eval $cmd &
