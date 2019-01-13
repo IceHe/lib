bc34b1c8f47d4b3cbc9017b56f8045a6c8bb9ddb
IceHe.xyz <icehe.xyz@qq.com>
2019-01-13 14:34:18 +0800

    Update snips/sort/tools.php

--- snips/sort/tools.php
+++ snips/sort/tools.php
@@ -1,17 +1,49 @@
 <?php
 
-function randAry(int $len = 10, int $min = 0, int $max = 100): array {
+function swap(array &$ary, int $idxA, int $idxB): void {
+    $tmp = $ary[$idxA];
+    $ary[$idxA] = $ary[$idxB];
+    $ary[$idxB] = $tmp;
+}
+
+function rndAry(int $len = 10, int $min = 0, int $max = 100): array {
     while ($len--) $ary[] = rand($min, $max);
     return $ary ?? [];
 }
 
-function aryToLine(array $ary): string {
+function ary2str(array $ary): string {
     $str = '[ ';
     for ($i = 0; $i < count($ary); $i++) {
         $str .= ($i ? ', ' : '') . $ary[$i];
     }
-    $str .= " ]\n";
+    $str .= " ]";
     return $str;
 }
 
-echo aryToLine(randAry());
+function chkSortedAry(array $ary): void {
+    $len = count($ary);
+    if ($len == 1) return;
+    for ($i = 0; $i < $len - 1; $i++) {
+        if ($ary[$i] < $ary[$i + 1]) {
+            line('not sorted!');
+            return;
+        }
+    }
+    line('sorted.');
+}
+
+function testSort(string $sortFnName): void {
+    $rndAry = rndAry();
+    line(ary2str($rndAry));
+    chkSortedAry($rndAry);
+
+    $sortedAry =  $sortFnName($rndAry);
+    line(ary2str($sortedAry));
+    chkSortedAry($sortedAry);
+}
+
+function line($str = ''): void {
+    echo "${str}\n";
+}
+
+//echo aryToLine(randAry());
