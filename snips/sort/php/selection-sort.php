ce96afba0e58e2954a416fba4b9e984522e83dea
IceHe.xyz <icehe.xyz@qq.com>
2019-01-14 01:02:26 +0800

    Add snips/sort/php/selection-sort.php

---
+++ snips/sort/php/selection-sort.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('./tools.php');
+
+function selectionSort(array &$ary): void {
+    $len = count($ary);
+    for ($i = 0; $i < $len; $i++) {
+        $minIdx = $i;
+        for ($j = $len - 1; $j > $i; $j--) {
+            if ($ary[$j] < $ary[$minIdx]) {
+                $minIdx = $j;
+            }
+        }
+        if ($minIdx != $i) swap($ary, $i, $minIdx);
+    }
+}
+
+testSort("selectionSort");
