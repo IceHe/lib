a0dfcd77c1687d5c2ca32f00bc0f119d17048072
IceHe.xyz <icehe.xyz@qq.com>
2019-01-17 00:58:20 +0800

    Add snips/sort/php/binary-search.php

---
+++ snips/sort/php/binary-search.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("./tools.php");
+require_once("./insertion-sort.php");
+
+function binarySearch($ary, $val): int {
+    $len = count($ary);
+    if ($len <= 0) {
+        return -1;
+    }
+
+    $beg = 0;
+    $end = $len - 1;
+    while ($beg <= $end) {
+        $privot = floor(($beg + $end) / 2);
+        if ($ary[$privot] === $val) {
+            return $privot;
+        } else if ($ary[$privot] < $val) {
+            $beg = $privot + 1;
+        } else { // $ary[$privot] > $val
+            $end = $privot - 1;
+        }
+    }
+    return -1;
+}
+
+line();
+$ary = rndAry();
+insertionSort($ary);
+line(ary2str($ary));
+
+$rndKey = mt_rand(0, count($ary) - 1);
+$searchVal = $ary[$rndKey];
+line("\$searchVal = $searchVal\n");
+line(binarySearch($ary, $searchVal));
