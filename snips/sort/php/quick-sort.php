3fbcca7be134a5db8033093340e8dbc31f204803
IceHe.xyz <icehe.xyz@qq.com>
2019-01-17 19:55:58 +0800

    Update snips/sort/php/quick-sort.php

--- snips/sort/php/quick-sort.php
+++ snips/sort/php/quick-sort.php
@@ -1,1 +1,35 @@
 <?php
+
+require_once('./tools.php');
+
+function quickSort(array &$ary): void {
+    $len = count($ary);
+    doQuickSort($ary, 0, $len - 1);
+}
+
+function doQuickSort(array &$ary, $beg, $end): void {
+    if ($beg >= $end) {
+        return;
+    }
+
+    $privot = floor(($beg + $end) / 2);
+    $front = $beg;
+    $rear = $end - 1;
+    swap($ary, $privot, $end);
+    while ($front <= $rear) {
+//        while ($front < $end - 1 && $ary[$front] < $ary[$end]) $front++;
+//        while ($rear > 0 && $ary[$rear] >= $ary[$end]) $rear--;
+        while ($front <= $end - 1 && $ary[$front] < $ary[$end]) $front++;
+        while ($rear >= 0 && $ary[$rear] >= $ary[$end]) $rear--;
+        swap($ary, $front, $rear);
+    }
+    swap($ary, $rear, $end);
+
+    $privot = floor(($beg + $end) / 2);
+    doQuickSort($ary, $beg, $privot);
+    doQuickSort($ary, $privot + 1, $end);
+
+}
+
+testSort("quickSort");
+
