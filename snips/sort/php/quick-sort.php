cc6b3a9ff29b4c99823e40d23ef1f062cb9e07d9
IceHe.xyz <icehe.xyz@qq.com>
2019-01-18 17:41:51 +0800

    Update snips/sort/php/quick-sort.php

--- snips/sort/php/quick-sort.php
+++ snips/sort/php/quick-sort.php
@@ -3,8 +3,7 @@
 require_once('./tools.php');
 
 function quickSort(array &$ary): void {
-    $len = count($ary);
-    doQuickSort($ary, 0, $len - 1);
+    doQuickSort($ary, 0, count($ary) - 1);
 }
 
 function doQuickSort(array &$ary, $beg, $end): void {
@@ -12,24 +11,53 @@ function doQuickSort(array &$ary, $beg, $end): void {
         return;
     }
 
-    $privot = floor(($beg + $end) / 2);
-    $front = $beg;
-    $rear = $end - 1;
-    swap($ary, $privot, $end);
-    while ($front <= $rear) {
-//        while ($front < $end - 1 && $ary[$front] < $ary[$end]) $front++;
-//        while ($rear > 0 && $ary[$rear] >= $ary[$end]) $rear--;
-        while ($front <= $end - 1 && $ary[$front] < $ary[$end]) $front++;
-        while ($rear >= 0 && $ary[$rear] >= $ary[$end]) $rear--;
-        swap($ary, $front, $rear);
+//    $privot = floor(($beg + $end) / 2);
+    $privot = mt_rand($beg, $end);
+    swap($ary, $beg, $privot);
+
+    $k = $beg;
+    for ($i = $beg + 1; $i <= $end; ++$i) {
+        if ($ary[$i] < $ary[$beg]) {
+            swap($ary, $i, ++$k);
+        }
     }
-    swap($ary, $rear, $end);
+
+    swap($ary, $beg, $k);
+
+    doQuickSort($ary, $beg, $k - 1);
+    doQuickSort($ary, $k + 1, $end);
+}
+
+function quickSort2(array &$ary): void {
+    $len = count($ary);
+    doQuickSort($ary, 0, $len - 1);
+}
+
+function doQuickSort2(array &$ary, $beg, $end): void {
+    if ($beg >= $end) {
+        return;
+    }
 
     $privot = floor(($beg + $end) / 2);
-    doQuickSort($ary, $beg, $privot);
+    swap($ary, $privot, $end);
+
+    $front = $beg;
+    $rear = $end - 1;
+    do {
+        while (/*$front <= $end - 1 &&*/ $ary[$front] < $ary[$end]) $front++;
+        while ($rear >= 0 && $ary[$rear] >= $ary[$end]) $rear--;
+        swap($ary, $front, $rear);
+    } while ($front <= $rear);
+    swap($ary, $front, $rear);
+
+    swap($ary, $privot, $end);
+
+    $privot = floor(($beg + $end) / 2);
+    doQuickSort($ary, $beg, $privot - 1);
     doQuickSort($ary, $privot + 1, $end);
 
 }
 
-testSort("quickSort");
+testSort("quickSort", 10);
+testSort("quickSort2", 10);
 
