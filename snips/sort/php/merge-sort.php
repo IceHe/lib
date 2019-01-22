04398628ac14d797d0c1e71ee7d7e8be39988456
IceHe.xyz <icehe.xyz@qq.com>
2019-01-23 00:59:24 +0800

    Update snips/sort/php/merge-sort.php

--- snips/sort/php/merge-sort.php
+++ snips/sort/php/merge-sort.php
@@ -29,33 +29,33 @@ function mergeSort(array &$ary): void {
     }
 }
 
-// wrong
-function mergeSort1(array &$ary): void {
-    $len = count($ary);
-    if ($len <= 1) {
-        return;
-    }
-
-    $privot = floor($len / 2);
-    $aryA = array_slice($ary, 0, $privot);
-    $aryB = array_slice($ary, $privot);
-    mergeSort1($aryA);
-    mergeSort1($aryB);
-
-    $k = 0;
-
-    while (!empty($aryA) && !empty($aryB)) {
-        if (reset($aryA) < reset($aryB)) {
-            $ary[$k++] = array_shift($aryA);
-        } else {
-            $ary[$k++] = array_shift($aryB);
-        }
-    }
-
-    while (!empty($aryB)) {
-        $ary[$k++] = array_shift($aryB);
-    }
-}
+//// wrong
+//function mergeSort1(array &$ary): void {
+//    $len = count($ary);
+//    if ($len <= 1) {
+//        return;
+//    }
+//
+//    $privot = floor($len / 2);
+//    $aryA = array_slice($ary, 0, $privot);
+//    $aryB = array_slice($ary, $privot);
+//    mergeSort1($aryA);
+//    mergeSort1($aryB);
+//
+//    $k = 0;
+//
+//    while (!empty($aryA) && !empty($aryB)) {
+//        if (reset($aryA) < reset($aryB)) {
+//            $ary[$k++] = array_shift($aryA);
+//        } else {
+//            $ary[$k++] = array_shift($aryB);
+//        }
+//    }
+//
+//    while (!empty($aryB)) {
+//        $ary[$k++] = array_shift($aryB);
+//    }
+//}
 
 function mergeSort2(array &$ary): void {
     doMergeSort2($ary, 0, count($ary) - 1);
@@ -68,7 +68,7 @@ function doMergeSort2(array &$ary, int $beg, int $end): void {
 
     $privot = floor(($beg + $end) / 2);
 
-    doMergeSort2($ary, 0, $privot);
+    doMergeSort2($ary, $beg, $privot);
     doMergeSort2($ary, $privot + 1, $end);
 
     $bakAry = $ary;
@@ -120,7 +120,7 @@ function doMergeSort3(array &$ary, int $beg, int $end): void {
 }
 
 testSort("mergeSort");
-testSort("mergeSort1");
+//testSort("mergeSort1");
 testSort("mergeSort2");
 //testSort("mergeSort3");
 
