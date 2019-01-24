3ecd73b4867181c94c71439e0c0b398c71e47009
IceHe.xyz <icehe.xyz@qq.com>
2019-01-24 15:44:10 +0800

    Add snips/sort/php/quick-sort-test.php

---
+++ snips/sort/php/quick-sort-test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('./tools.php');
+
+function quickSort(array &$ary): void {
+}
+
+function doQuickSort(array &$ary, $first, $last): void {
+}
+
+testSort("quickSort", 10);
+//testSort("quickSort2", 10);
+
