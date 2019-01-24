3bc78e8385313f24cbfc0120cb4adc55553b201b
IceHe.xyz <icehe.xyz@qq.com>
2019-01-24 15:44:08 +0800

    Add snips/sort/php/merge-sort-test.php

---
+++ snips/sort/php/merge-sort-test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('./tools.php');
+
+function mergeSort(array &$ary): void {
+}
+
+testSort("mergeSort");
+//testSort("mergeSort1");
+//testSort("mergeSort2");
+//testSort("mergeSort3");
+
