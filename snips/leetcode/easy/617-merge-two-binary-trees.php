a50a34a1e8c8c364eb184fc8cb8f0e997985f67f
IceHe.xyz <icehe.xyz@qq.com>
2019-02-15 16:23:26 +0800

    .

--- snips/leetcode/easy/617-merge-two-binary-trees.php
+++ snips/leetcode/easy/617-merge-two-binary-trees.php
@@ -34,14 +34,44 @@ class Solution {
     }
 }
 
+class Solution2 {
+
+    /**
+     * @param TreeNode $t1
+     * @param TreeNode $t2
+     * @return TreeNode
+     */
+    function mergeTrees($t1, $t2) {
+        if ($t1 === null) {
+            return $t2;
+        }
+        if ($t2 === null) {
+            return $t1;
+        }
+
+        $t1->val = ($t1->val ?? 0) + ($t2->val ?? 0);
+
+        $t1->left = $this->mergeTrees($t1->left ?? null, $t2->left ?? null);
+        $t1->right = $this->mergeTrees($t1->right ?? null, $t2->right ?? null);
+
+        return $t1;
+    }
+}
+
 $t1 = TreeNode::build([1,3,2,5]);
 $t2 = TreeNode::build([2,1,3,null,4,null,7]);
-
-$solution = new Solution();
-$result = $solution->mergeTrees($t1, $t2);
 
 TreeNode::traverse($t1);
 echo "----------\n";
 TreeNode::traverse($t2);
 echo "----------\n";
+
+$solution = new Solution();
+$result = $solution->mergeTrees($t1, $t2);
 TreeNode::traverse($result);
+echo "----------\n";
+
+$solution2 = new Solution2();
+$result2 = $solution2->mergeTrees($t1, $t2);
+TreeNode::traverse($result2);
+echo "----------\n";
