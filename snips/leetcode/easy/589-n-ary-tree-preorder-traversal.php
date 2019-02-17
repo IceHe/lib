2330de2bfd4a9b9bb74808b79d7d08631c6182d1
IceHe.xyz <icehe.xyz@qq.com>
2019-02-17 20:19:19 +0800

    .

--- snips/leetcode/easy/589-n-ary-tree-preorder-traversal.php
+++ snips/leetcode/easy/589-n-ary-tree-preorder-traversal.php
@@ -17,8 +17,42 @@ class Node {
 }
 */
 
+//Recursion
 //Runtime: 724 ms, faster than 100.00% of PHP online submissions for N-ary Tree Preorder Traversal.
 //Memory Usage: 144.8 MB, less than 100.00% of PHP online submissions for N-ary Tree Preorder Traversal.
+
+//class Solution {
+//
+//    /**
+//     * @param Node $root
+//     * @return Integer[]
+//     */
+//    function preorder($root) {
+//        if ($root === null) {
+//            return [];
+//        }
+//
+//        $track = [];
+//        $this->doPreorder($root, $track);
+//        return $track;
+//    }
+//
+//    private function doPreorder(?Node $root, array &$track) {
+//        if ($root === null) {
+//            return;
+//        }
+//
+//        $track[] = $root->val;
+//
+//        foreach ($root->children as $child) {
+//            $this->doPreorder($child, $track);
+//        }
+//    }
+//}
+
+//Iteration
+//Runtime: 672 ms, faster than 100.00% of PHP online submissions for N-ary Tree Preorder Traversal.
+//Memory Usage: 145 MB, less than 100.00% of PHP online submissions for N-ary Tree Preorder Traversal.
 
 class Solution {
 
@@ -31,20 +65,22 @@ class Solution {
             return [];
         }
 
-        $track = [];
-        $this->doPreorder($root, $track);
-        return $track;
-    }
+        $result = [];
+        $stack = [$root];
 
-    private function doPreorder(?Node $root, array &$track) {
-        if ($root === null) {
-            return;
+        while (!empty($stack)) {
+            $node = array_pop($stack);
+//            if ($node === null) {
+//                continue;
+//            }
+
+            $result[] = $node->val;
+
+            foreach (array_reverse($node->children) as $child) {
+                array_push($stack, $child);
+            }
         }
 
-        $track[] = $root->val;
-
-        foreach ($root->children as $child) {
-            $this->doPreorder($child, $track);
-        }
+        return $result;
     }
 }
--- snips/leetcode/easy/590-n-ary-tree-postorder-traversal.php
+++ snips/leetcode/easy/590-n-ary-tree-postorder-traversal.php
@@ -17,6 +17,77 @@ class Node {
 }
 */
 
+//Recursion
+//Runtime: 628 ms, faster than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
+//Memory Usage: 145.2 MB, less than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
+
+//class Solution {
+//
+//    /**
+//     * @param Node $root
+//     * @return Integer[]
+//     */
+//    function postorder($root) {
+//        if ($root === null) {
+//            return [];
+//        }
+//
+//        $track = [];
+//        $this->doPostorder($root, $track);
+//        return $track;
+//    }
+//
+//    private function doPostorder(?Node $node, array &$track): void {
+//        if ($node === null) {
+//            return;
+//        }
+//
+//        foreach ($node->children as $child) {
+//            $this->doPostorder($child, $track);
+//        }
+//
+//        $track[] = $node->val;
+//    }
+//}
+
+//Iteration
+//Runtime: 608 ms, faster than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
+//Memory Usage: 145 MB, less than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
+
+//class Solution {
+//
+//    /**
+//     * @param Node $root
+//     * @return Integer[]
+//     */
+//    function postorder($root) {
+//        if ($root === null) {
+//            return [];
+//        }
+//
+//        $track = [];
+//        $stack = [$root];
+//
+//        while (!empty($stack)) {
+//            $peep = end($stack);
+//            if (empty($peep->children)) {
+//                array_pop($stack);
+//                $track[] = $peep->val;
+//            } else {
+//                foreach (array_reverse($peep->children) as $child) {
+//                    array_push($stack, $child);
+//                }
+//                $peep->children = null;
+//            }
+//        }
+//
+//        return $track;
+//    }
+//}
+
+//Iteration2
+//Runtime: 552 ms, faster than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
+//Memory Usage: 145 MB, less than 100.00% of PHP online submissions for N-ary Tree Postorder Traversal.
 
 class Solution {
 
@@ -25,6 +96,25 @@ class Solution {
      * @return Integer[]
      */
     function postorder($root) {
+        if ($root === null) {
+            return [];
+        }
 
+        $track = [];
+        $stack = [$root];
+
+        while (!empty($stack)) {
+            $node = array_pop($stack);
+//            if ($node === null) {
+//                continue;
+//            }
+
+            $track[] = $node->val;
+            foreach ($node->children as $child) {
+                array_push($stack, $child);
+            }
+        }
+
+        return array_reverse($track);
     }
 }
