ed72e6c0877fb0a59c8f5a8fb9a3a43db9a6f5bc
IceHe.xyz <icehe.xyz@qq.com>
2019-02-24 16:45:48 +0800

    .

--- snips/leetcode/easy/893-groups-of-special-equivalent-strings.php
+++ snips/leetcode/easy/893-groups-of-special-equivalent-strings.php
@@ -42,8 +42,8 @@
 //    }
 //}
 
-//参考原题 solution 之后尝试一下
-//Runtime: 24 ms, faster than 100.00% of PHP online submissions for Groups of Special-Equivalent Strings.
+//优化了不必要的步骤
+//Runtime: 20 ms, faster than 100.00% of PHP online submissions for Groups of Special-Equivalent Strings.
 //Memory Usage: 16.5 MB, less than 100.00% of PHP online submissions for Groups of Special-Equivalent Strings.
 
 class Solution {
@@ -53,36 +53,70 @@ class Solution {
      * @return Integer
      */
     function numSpecialEquivGroups($A) {
-        $collection = [];
-        foreach ($A as $a) {
-            $counter = [];
+        foreach ($A as &$a) {
+            $evenChars = [];
+            $oddChars = [];
 
             $len = strlen($a);
             for ($i = 0; $i < $len; $i++) {
-                $index = ord($a{$i}) - ord('a') + ($i % 2) * 26;
-
-                if (!isset($counter[$index])) {
-                    $counter[$index] = 1;
+                if ($i % 2 === 0) {
+                    $evenChars[] = $a{$i};
                 } else {
-                    $counter[$index]++;
+                    $oddChars[] = $a{$i};
                 }
             }
 
-            $str = $this->counter2str($counter);
-            $collection[] = $str;
+            sort($evenChars);
+            sort($oddChars);
+
+            $a = implode('', $evenChars).implode('', $oddChars);
         }
 
-        return count(array_unique($collection));
-    }
-
-    private function counter2str(array $counter) {
-        $str = '';
-        for ($i = 0; $i < 52; $i++) {
-            $str .= $counter[$i] ?? '0';
-        }
-        return $str;
+        return count(array_unique($A));
     }
 }
+
+//参考原题 solution 之后尝试一下
+//Runtime: 24 ms, faster than 100.00% of PHP online submissions for Groups of Special-Equivalent Strings.
+//Memory Usage: 16.5 MB, less than 100.00% of PHP online submissions for Groups of Special-Equivalent Strings.
+
+//class Solution {
+//
+//    /**
+//     * @param String[] $A
+//     * @return Integer
+//     */
+//    function numSpecialEquivGroups($A) {
+//        $collection = [];
+//        foreach ($A as $a) {
+//            $counter = [];
+//
+//            $len = strlen($a);
+//            for ($i = 0; $i < $len; $i++) {
+//                $index = ord($a{$i}) - ord('a') + ($i % 2) * 26;
+//
+//                if (!isset($counter[$index])) {
+//                    $counter[$index] = 1;
+//                } else {
+//                    $counter[$index]++;
+//                }
+//            }
+//
+//            $str = $this->counter2str($counter);
+//            $collection[] = $str;
+//        }
+//
+//        return count(array_unique($collection));
+//    }
+//
+//    private function counter2str(array $counter) {
+//        $str = '';
+//        for ($i = 0; $i < 52; $i++) {
+//            $str .= $counter[$i] ?? '0';
+//        }
+//        return $str;
+//    }
+//}
 
 $solution = new Solution();
 
