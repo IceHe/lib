da9ee21ffb33671c258c90b83aa48f7f3d6ef555
IceHe.xyz <icehe.xyz@qq.com>
2019-04-14 17:19:33 +0800

    2019-04-14 17:19:33

---
+++ _lru/git/change-author-info-of-commits.tmpl.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# References
+# - Changing author info - GitHub Help : https://help.github.com/en/articles/changing-author-info
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="x@icehe.me"
+CORRECT_NAME="IceHe"
+CORRECT_EMAIL="icehe.me@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' -f --tag-name-filter cat -- --branches --tags
+
