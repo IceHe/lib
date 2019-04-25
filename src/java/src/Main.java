120c67fd8aa3d98c814bee26a66b884e9595d0db
IceHe.xyz <icehe.xyz@qq.com>
2019-04-26 00:50:07 +0800

    Update src/java/src/Main.java

--- src/java/src/Main.java
+++ src/java/src/Main.java
@@ -1,6 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+//        list.add("2");
+
+        for (String item : list) {
+            if ("1".equals(item)) {
+                list.remove(item);
+            }
+        }
+
         System.out.println("Hello World!");
     }
 }
