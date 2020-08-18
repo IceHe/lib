fc4e8e6df3643be7bf513537ca74368058c818a0
IceHe.xyz <icehe.xyz@qq.com>
2020-08-18 20:06:35 +0800

    2020-08-18 20:06:35 Tue

---
+++ books/src/understand-jvm/NewGenerationMinorGC.java
@@ -0,0 +1,17 @@
+/**
+ * javac NewGenerationMinorGC.java
+ * java -XX:+UseSerialGC -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 NewGenerationMinorGC > NewGenerationMinorGC.out 2>&1
+ */
+public class NewGenerationMinorGC {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        // 出现一次 Minor GC
+        allocation4 = new byte[4 * _1MB];
+    }
+}
