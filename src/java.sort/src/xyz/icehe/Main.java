ea4a84c2ae446823fb164cb93f9dd43f001d56cb
IceHe.xyz <icehe.xyz@qq.com>
2021-01-16 12:35:05 +0800

    2021-01-16 12:35:05 Sat

--- src/java.sort/src/xyz/icehe/Main.java
+++ src/java.sort/src/xyz/icehe/Main.java
@@ -3,6 +3,7 @@ package xyz.icehe;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("test");
     }
 }
