479ad07676b426e96c523ef5a278e8eb67a664ee
IceHe.xyz <icehe.xyz@qq.com>
2021-02-09 17:02:57 +0800

    2021-02-09 17:02:57 Tue

--- sort/java.sort/src/xyz/icehe/Main.java
+++ sort/java.sort/src/xyz/icehe/Main.java
@@ -3,6 +3,37 @@ package xyz.icehe;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Sorting algorithms");
+        //System.out.println(2.0e-6 * 100000000.1);
+        //System.out.println((1+2.236) / 2);
+        //System.out.println(1+2+3+4.0);
+        //System.out.println(4.1 >= 4);
+        //System.out.println(1+2+"3");
+
+        //int[] a = new int[10];
+        //for (int i = 0; i < 10; i++) {
+        //    a[i] = 9 - i;
+        //}
+        //for (int i = 0; i < 10; i++) {
+        //    a[i] = a[a[i]];
+        //}
+        //for (int i = 0; i < 10; i++) {
+        //    System.out.println(a[i]);
+        //}
+
+        //int[][] b = new int[][] {{0, 1, 2}, {3, 4, 5}};
+        //for (int j = 0; j < b[0].length; j++) {
+        //    for (int k = 0; k < b.length; k++) {
+        //        System.out.print(b[k][j] + " ");
+        //    }
+        //    System.out.println();
+        //}
+
+        //double t = 9.0;
+        //while (Math.abs(t - 9.0 / t) > .001) {
+        //    t = (9.0 / t + t) / 2.0;
+        //}
+        //System.out.printf("%.5f\n", t);
+
+        System.out.println((char)('a' + 4));
     }
 }
