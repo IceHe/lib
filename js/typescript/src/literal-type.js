80f09bb9cf3e5c81c0a938ae0086d18a13c23ba3
IceHe.xyz <icehe.xyz@qq.com>
2021-10-05 10:56:32 +0800

    2021-10-05 10:56:32 Tue

---
+++ js/typescript/src/literal-type.js
@@ -0,0 +1,7 @@
+function print123(s) {
+    console.log(s);
+}
+print123("one");
+print123("two");
+print123("three");
+// print123("four");
---
+++ js/typescript/src/literal-type.ts
@@ -0,0 +1,8 @@
+function print123(s: "one" | "two" | "three") {
+    console.log(s);
+}
+
+print123("one");
+print123("two");
+print123("three");
+// print123("four");
