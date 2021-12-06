d84252a959f538de2edccc6553eec92caefba470
IceHe.xyz <icehe.xyz@qq.com>
2021-12-06 17:27:23 +0800

    2021-12-06 17:27:23 Mon

--- git/change-author-info-of-commits.tmpl.sh
+++ git/change-author-info-of-commits.tmpl.sh
@@ -5,18 +5,31 @@
 
 git filter-branch --env-filter '
 
-OLD_EMAIL="icehe.me@qq.com"
-CORRECT_NAME="IceHe"
+CORRECT_NAME="IceHe.xyz"
 CORRECT_EMAIL="icehe.xyz@qq.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
+OLD_EMAILS=(
+    "example@example.com"
+    "icehe.xyz@qq.com"
+)
+
+for OLD_EMAIL in ${OLD_EMAILS[@]}; do
+    if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+    then
+        export GIT_COMMITTER_NAME="$CORRECT_NAME"
+        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    fi
+    if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+    then
+        export GIT_AUTHOR_NAME="$CORRECT_NAME"
+        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    fi
+done
+
 ' -f --tag-name-filter cat -- --branches --tags
+
+# After finishing, execute the following command to check
+#
+# ```bash
+# git log | grep -E '^Author' | sort | uniq
+# ```
