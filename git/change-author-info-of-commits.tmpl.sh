21124b2788a434c2ef408dda0ae0b57b51a4ed52
IceHe.xyz <icehe.xyz@qq.com>
2021-12-06 22:11:42 +0800

    2021-12-06 22:11:42 Mon

--- git/change-author-info-of-commits.tmpl.sh
+++ git/change-author-info-of-commits.tmpl.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
-# References
-# - Changing author info - GitHub Help : https://help.github.com/en/articles/changing-author-info
+# Reference: [Changing author info - GitHub Help](https://help.github.com/en/articles/changing-author-info)
 
 git filter-branch --env-filter '
 
-CORRECT_NAME="IceHe.xyz"
-CORRECT_EMAIL="icehe.xyz@qq.com"
+    CORRECT_NAME="IceHe.xyz"
+    CORRECT_EMAIL="icehe.xyz@qq.com"
 
-OLD_EMAILS=(
-    "example@example.com"
-    "icehe.xyz@qq.com"
-)
+    OLD_EMAILS=(
+        "example@example.com"
+        "icehe.xyz@qq.com"
+    )
 
-for OLD_EMAIL in ${OLD_EMAILS[@]}; do
-    if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-    then
-        export GIT_COMMITTER_NAME="$CORRECT_NAME"
-        export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-    fi
-    if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-    then
-        export GIT_AUTHOR_NAME="$CORRECT_NAME"
-        export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-    fi
-done
+    for OLD_EMAIL in ${OLD_EMAILS[@]}; do
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_COMMITTER_NAME="$CORRECT_NAME"
+            export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+        then
+            export GIT_AUTHOR_NAME="$CORRECT_NAME"
+            export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        fi
+    done
 
 ' -f --tag-name-filter cat -- --branches --tags
 
